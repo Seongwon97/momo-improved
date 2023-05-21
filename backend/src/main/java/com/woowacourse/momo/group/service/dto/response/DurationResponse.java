55f41335bffcc1af24b0bd54946feb489f1c48ff
Seongwon97 <osw970325@naver.com>
2023-05-21 16:15:40 +0900

    fix: 누락돤 DTO의 직렬화 설정 추가

--- backend/src/main/java/com/woowacourse/momo/group/service/dto/response/DurationResponse.java
+++ backend/src/main/java/com/woowacourse/momo/group/service/dto/response/DurationResponse.java
@@ -2,28 +2,25 @@ package com.woowacourse.momo.group.service.dto.response;
 
 import java.time.LocalDate;
 
-import org.springframework.format.annotation.DateTimeFormat;
-
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import com.woowacourse.momo.global.config.LocalDateSerializer;
 
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 public class DurationResponse {
 
-    @DateTimeFormat
     @JsonSerialize(using = LocalDateSerializer.class)
     @JsonDeserialize(using = LocalDateDeserializer.class)
     private LocalDate start;
 
-    @DateTimeFormat
     @JsonSerialize(using = LocalDateSerializer.class)
     @JsonDeserialize(using = LocalDateDeserializer.class)
     private LocalDate end;
